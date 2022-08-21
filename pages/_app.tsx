3eedb1292fe800abb2e8b20d3bf0847a7677c708
Lewis Turner <lewis@lewiss-mbp.home>
2022-08-21 16:31:46 +0100

    added blueprintjs

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import '../node_modules/normalize.css/normalize.css'
+import '../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css'
+import '../node_modules/@blueprintjs/core/lib/css/blueprint.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
